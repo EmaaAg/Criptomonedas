f7ee5bf285534f7676e5513e5b05dae4e972eaa4
EmaaAg <emaguilera@alumno.unlam.edu.ar>
2024-06-08 12:16:25 -0300

    a

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -48,6 +48,55 @@ public class Menu {
         sc.close();
 	}
 	
+	public static void menuTrader() {
+		
+		String mensaje = 
+				"1) Comprar Criptomonedas.\n" +
+                "2) Vender Criptomonedas.\n" +
+                "3) Consultar Criptomoneda.\n" +
+                "4) Recomendar Criptomonedas.\n" +
+                "5) Consultar estado actual del mercado.\n" +
+                "6) Visualizar archivo de transacciones (histórico).\n" +
+                "7) Salir.\n";
+        Scanner sc = new Scanner(System.in);
+        int opcion = obtenerOpcion(mensaje, 1, 7, sc); 
+
+        switch (opcion) {
+	        case 1:
+	            //comprarCriptomoneda();
+	            System.out.println("Opción Comprar Criptomoneda seleccionada.");
+	            break;
+	        case 2:
+	            //venderCriptomoneda();
+	            System.out.println("Opción Vender Criptomoneda seleccionada.");
+	            break;
+	        case 3:
+	            //consultarCriptomoneda();
+	            System.out.println("Opción Consultar Criptomoneda seleccionada.");
+	            break;
+	        case 4:
+	            //recomendarCriptomoneda();
+	            System.out.println("Opción Recomendar Criptomoneda seleccionada.");
+	            break;
+	        case 5:
+	            //consultarEstadoMercado();
+	            System.out.println("Opción Consultar estado actual del mercado seleccionada.");
+	            break;
+	        case 6:
+	        	//archivoTransacciones();
+	        	System.out.println("Opción Visualizar archivo Transacciones.");
+	        	break;
+	        case 7:
+	            System.out.println("Saliendo del programa.");
+                System.exit(0);
+	            break;
+	        default:
+	            System.out.println("Opción no válida.");
+	            break;
+        }
+        sc.close();
+	}
+	
     public static int obtenerOpcion(String mensaje, int min, int max, Scanner scanner) {
 
         int opcion = 0;
@@ -55,7 +104,7 @@ public class Menu {
         System.out.println(mensaje);
 
         do {
-            System.out.print("Ingrese su opción: ");
+            System.out.print("Ingrese su opción("+ min + " - " + max + "): ");
             if (scanner.hasNextInt()) {
                 opcion = scanner.nextInt();
             } else {
